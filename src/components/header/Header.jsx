26728f039c24087cbd54bae4137a01cfa4764736
AdnanAAzad <adnan.al.azad@gmail.com>
2022-07-21 15:26:14 -0400

    Update

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -10,7 +10,7 @@ const Header = () => {
         <h5>Hello I am</h5>
         <h1>ADNAN AZAD</h1>
         <h5 className="text-light">
-          WEB DEVELOPMENT ENGINEER AT ROTARACT CLUB OF TORONTO DANFORTH
+          FRONT-END WEB DEVELOPMENT ENGINEER AT ROTARACT CLUB OF TORONTO DANFORTH
         </h5>
         <CTA />
         <HeaderSocials />
