2b12d2e2861f31035356df3d9a030d70ba075ac4
AdnanAAzad <adnan.al.azad@gmail.com>
2023-02-24 14:26:34 -0500

    Updates

---
+++ precache-manifest.d7a78acd09baf094d71fb29cbb664e92.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = [
+  {
+    "revision": "963113b2ded9a54c1d22",
+    "url": "/static/css/main.cb86eddf.chunk.css"
+  },
+  {
+    "revision": "963113b2ded9a54c1d22",
+    "url": "/static/js/main.272092ef.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "ffe42d2f5de6f546bdca",
+    "url": "/static/js/2.5fbcd574.chunk.js"
+  },
+  {
+    "revision": "a7a9c7e40590a304ea9e1739a00bcb4a",
+    "url": "/static/media/AdnanAzadResume.a7a9c7e4.pdf"
+  },
+  {
+    "revision": "e07c27cd79b737b6fd5907bb7e34e121",
+    "url": "/static/media/me.e07c27cd.png"
+  },
+  {
+    "revision": "1762a9509a4a91a9d29bfe0f81c4bbca",
+    "url": "/static/media/me-about.1762a950.PNG"
+  },
+  {
+    "revision": "bc0f182db98ff249fccd9b6b9b889052",
+    "url": "/static/media/pokemon.bc0f182d.png"
+  },
+  {
+    "revision": "9d80c35c2bc339a70099ebec7ec06e36",
+    "url": "/static/media/Python-Code-Image.9d80c35c.jpg"
+  },
+  {
+    "revision": "8a97477303624dd291b5f082b325aeaf",
+    "url": "/static/media/portfolio5.8a974773.png"
+  },
+  {
+    "revision": "fa82be3c13052bc60855385178504976",
+    "url": "/static/media/storage_mgnt_system.fa82be3c.PNG"
+  },
+  {
+    "revision": "8f88f0b344ad1a2b45d926f79175f29a",
+    "url": "/static/media/portfoliosite.8f88f0b3.png"
+  },
+  {
+    "revision": "7c4083ddc91577be43137d25c4ee8d06",
+    "url": "/static/media/joblisterapp.7c4083dd.png"
+  },
+  {
+    "revision": "7405d8cc834b6ddec31ec195be0d2bb2",
+    "url": "/static/media/rotaractcluboftorontodanforth.7405d8cc.png"
+  },
+  {
+    "revision": "d26a31f82b7306ba26e8f44d97c9aba6",
+    "url": "/static/media/sumaiyagroup.d26a31f8.jpg"
+  },
+  {
+    "revision": "3631b02a415c047b0a3adace5c7a6fb8",
+    "url": "/static/media/wordpresssite.3631b02a.jpg"
+  },
+  {
+    "revision": "39181cadadbf500aa1e0a3bac073b815",
+    "url": "/index.html"
+  }
+];
