4b3aa6d7efba46a6f1603142d1126333aa7c2054
AdnanAAzad <adnan.al.azad@gmail.com>
2023-03-11 11:09:46 -0500

    Update Merge

--- src/index.js
+++ src/index.js
@@ -3,4 +3,8 @@ import App from './App'
 import ReactDOM from 'react-dom'
 import './index.css'
 
+<<<<<<< HEAD
 ReactDOM.render(<App/>, document.querySelector("#root"))    
+=======
+ReactDOM.render(<App/>, document.querySelector("#root"))
+>>>>>>> parent of 38b8903 (Updated Colo Cheme)
